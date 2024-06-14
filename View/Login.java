467a20a8adc9de0a1f72d5925131b2e8a2a1d34e
goisleandro <goisleandro@gmail.com>
2024-06-13 21:21:11 -0300

    interface leandro

---
+++ View/Login.java
@@ -0,0 +1,45 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame {
+
+    private JPanel JPanelLogin1;
+    private JLabel lbLogin;
+    private JTextField textField1;
+    private JLabel lbSenha;
+    private JButton btnCancelar;
+    private JLabel lbEmail;
+    private JButton btnOK;
+    private JPasswordField passwordField1;
+
+    public Login() {
+
+        setContentPane(JPanelLogin1);
+        setTitle("Login");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(450, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        btnOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Principal();
+                dispose();
+            }
+        });
+        btnCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Login login = new Login();
+    }
+}
---
+++ View/Principal.java
@@ -0,0 +1,66 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Principal extends JFrame{
+    private JPanel JPPrincipal;
+    private JTextField tfGramado;
+    private JTextField tfCheckin;
+    private JTextField tfCheckout;
+    private JTextField tfHospedes;
+    private JButton btnBuscar;
+    private JPanel JPBuscar;
+    private JPanel JPImagem;
+    private JLabel lbImagem;
+
+    public Principal () {
+        setContentPane(JPPrincipal);
+        setTitle("Airbnb | Locações");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(700, 500);
+        setLocationRelativeTo(null);
+
+        JPImagem.setVisible(false);
+
+        ImageIcon lbImagemIcon = new ImageIcon("resources/buscar_escuro.png");
+        lbImagem.setIcon(lbImagemIcon);
+        lbImagem.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        lbImagem.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new teste();
+                dispose();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                ImageIcon lbImagemHoverIcon = new ImageIcon("resources/buscar.png");
+                lbImagem.setIcon(lbImagemHoverIcon);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                lbImagem.setIcon(lbImagemIcon);
+            }
+        });
+
+        btnBuscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JPImagem.setVisible(true);
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+    }
+}
